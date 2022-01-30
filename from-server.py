89f9e30e57145bbef8cc097a9d845759f8aa1e5c
alexanderciuffreda <ac053@hdm-stuttgart.de>
2022-01-31 00:16:14 +0100

    typo

--- from-server.py
+++ from-server.py
@@ -12,19 +12,76 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Penguin example using TFX and XGBoost.
 
-Run pipeline:
-$ python examples/xgboost_penguins/penguin_pipeline_local.py
-"""
-
+from tfx.orchestration.airflow.airflow_dag_runner import AirflowPipelineConfig
 import os
 from pathlib import Path
 from typing import List, Text
-import datetime
+
 from absl import logging
 from tfx import v1 as tfx
 from tfx.orchestration.airflow.airflow_runner import AirflowDAGRunner
+import datetime
+
+_pipeline_name = 'engine_xgboost_local'
+
+#airflow configs
+_airflow_config = {
+    'schedule_interval': None,
+    'start_date': datetime.datetime(2019, 1, 1),
+}
+
+# Feel free to customize as needed.
+_root = Path(__file__).parent
+_data_root = str(_root / 'data')
+
+# Python module file to inject customized logic into TFX components.
+_module_file = str(_root / 'utils.py')
+
+# Pipeline artifact locations. You can store these files anywhere on your local filesystem.
+_tfx_root = Path(os.environ['HOME']) / 'tfx'
+_pipeline_root = str(_tfx_root / 'pipelines' / _pipeline_name)
+_metadata_path = str(_tfx_root / 'metadata' / _pipeline_name / 'metadata.db')
+
+# Pipeline arguments for Beam powered Components.
+_beam_pipeline_args = [
+    '--direct_running_mode=multi_threading',
+    # 0 means auto-detect based on on the number of CPUs available
+    # during execution time.
+    '--direct_num_workers=1',
+]
+
+
+def create_pipeline(
+    pipeline_name: Text,
+    pipeline_root: Text,
+    data_root: Text,
+    module_file: Text,
+                                                                                                                                                                                                 
+# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from tfx.orchestration.airflow.airflow_dag_runner import AirflowPipelineConfig
+import os
+from pathlib import Path
+from typing import List, Text
+
+from absl import logging
+from tfx import v1 as tfx
+from tfx.orchestration.airflow.airflow_runner import AirflowDAGRunner
+import datetime
 
 _pipeline_name = 'engine_xgboost_local'
 
@@ -107,6 +164,7 @@ def create_pipeline(
       custom_config=trainer_custom_config,
   )
 
+
   return tfx.dsl.Pipeline(
       pipeline_name=pipeline_name,
       pipeline_root=pipeline_root,
@@ -124,7 +182,7 @@ def create_pipeline(
   )
 
 
-      
+
 # 'DAG' below need to be kept for Airflow to detect dag.
 DAG = AirflowDAGRunner(AirflowPipelineConfig(_airflow_config)).run(
     create_pipeline(pipeline_name=_pipeline_name,
@@ -132,4 +190,4 @@ DAG = AirflowDAGRunner(AirflowPipelineConfig(_airflow_config)).run(
                       data_root=_data_root,
                       module_file=_module_file,
                       metadata_path=_metadata_path,
-                      beam_pipeline_args=_beam_pipeline_args))
+                      beam_pipeline_args=_beam_pipeline_args))                                                                                                                                                                            
